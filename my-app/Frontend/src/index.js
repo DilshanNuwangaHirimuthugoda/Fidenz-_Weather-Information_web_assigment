75e2667c232b99205fb3bf41d673e1d8bef6d774
DilshanNuwangaHirimuthugoda <dilshannuwangahirimuthugoda@gmail.com>
2025-08-31 18:09:10 +0530

    Add initial React entry point with Auth0 integration
    Created index.js as the main entry point for the React app, integrating Auth0 authentication using Auth0Provider. Also added an empty index.css file.

---
+++ my-app/Frontend/src/index.js
@@ -0,0 +1,41 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+// index.js - Main entry point
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import './App.css';
+
+const domain = "dev-hig1jvsx81zjff84.us.auth0.com";
+const clientId = "CrN6TVR3hVQcQtNyJWtB6KiTc7dzgijG";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
