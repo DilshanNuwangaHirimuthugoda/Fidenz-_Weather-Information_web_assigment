a01f5623e5d2999f8c6eefd12f15098831e01385
DilshanNuwangaHirimuthugoda <dilshannuwangahirimuthugoda@gmail.com>
2025-08-31 18:25:53 +0530

    update

--- my-app/Frontend/src/components/BackgroundClouds.js
+++ my-app/Frontend/src/components/BackgroundClouds.js
@@ -1,31 +1,4 @@
-// // components/BackgroundClouds.js
-// import React from 'react';
 
-// const BackgroundClouds = () => {
-//   return (
-//     <>
-//       <div className="cloud-bg cloud-1"></div>
-//       <div className="cloud-bg cloud-2"></div>
-//       <div className="cloud-bg cloud-3"></div>
-//       <div className="cloud-bg cloud-4"></div>
-//     </>
-//   );
-// };
-
-// export default BackgroundClouds;
-
-
-
-
-
-
-
-
-
-
-
-
-// components/BackgroundClouds.js
 import React from 'react';
 
 const BackgroundClouds = () => {
