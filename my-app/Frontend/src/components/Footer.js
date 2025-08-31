6bbf37ff32c2a5f5c636c4f7b3f7c08055272342
DilshanNuwangaHirimuthugoda <dilshannuwangahirimuthugoda@gmail.com>
2025-08-31 18:07:50 +0530

    Add BackgroundClouds and Footer components
    Introduced BackgroundClouds for animated cloud backgrounds and Footer for displaying company information. Footer year updated to 2025.

---
+++ my-app/Frontend/src/components/Footer.js
@@ -0,0 +1,36 @@
+// // components/Footer.js
+// import React from 'react';
+
+// const Footer = () => {
+//   return (
+//     <div className="footer">2021 Fidenz Technologies</div>
+//   );
+// };
+
+// export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">2025 Fidenz Technologies</div>
+  );
+};
+
+export default Footer;
